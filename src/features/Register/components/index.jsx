9cfeda67a44033281f2e843552a220ab2b704e17
Tantai231204 <taintse184659@fpt.edu.vn>
2024-09-18 20:15:04 +0700

    tai

---
+++ src/features/Register/components/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss'
+
+Register.propTypes = {
+    
+};
+
+function Register(props) {
+    return (
+        <div>
+            Register
+        </div>
+    );
+}
+
+export default Register;
