000b88305a36b3373a115cad8614eb10b7a5220c
BTShinC <sangp3328@gmail.com>
2024-09-21 01:53:55 +0700

    Finish setup header

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import HomePage from "./pages/home";
 function App() {
     const router = createBrowserRouter([
         {
-          path: "/",
+          path: "/home",
           element: <HomePage/>,
         },
         
