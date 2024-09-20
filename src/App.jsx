3e7e057fb3bacbce45e2829847fc9bebad1e9768
Sendudu2311 <cattpbse184684@fpt.edu.vn>
2024-09-20 23:17:19 +0700

    modified file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './components/login/login';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LoginForm from './pages/login/login';
+import UserInfo from './pages/userinfo/userinfo';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+        </Routes>
+        
+        
+        <div>
+          <Link to="/userinfo">Test User Info Page</Link> <br></br>
+          <Link to="/login">Test Login Page</Link>
+        </div>
+      </div>
     </Router>
   );
 }
