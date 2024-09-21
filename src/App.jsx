0fd5f76eb9c5284d1479ba15e1e506a0cfd1411f
Sendudu2311 <cattpbse184684@fpt.edu.vn>
2024-09-22 01:46:11 +0700

    added preview for addfish and userinfo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
+import UserInfoPage from "./pages/userinfo";
+import AddFishPage from "./pages/addfish";
 
 
 
@@ -10,13 +12,24 @@ function App() {
     const router = createBrowserRouter([
         {
           path: "/",
-          element: <HomePage/>,
+          element: <LoginPage/>,
         },
 
         {
           path: "/login",
           element: <LoginPage/>,
         },
+
+        {
+          path: "/userinfo", 
+          element: <UserInfoPage/>,
+        },
+
+        {
+          path: "/addfish",
+          element: <AddFishPage/>,
+        },
+        
     
       ]);
       return <RouterProvider router={router} />;
