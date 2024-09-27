a685da8db10a32b917d8f4307e8344eba8bf5752
Sendudu2311 <cattpbse184684@fpt.edu.vn>
2024-09-27 08:55:42 +0700

    fixed about page css

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -16,12 +16,12 @@ function AboutPage() {
                     </div>
                 </div>
                 <div className="page-content">
-                <h1 className="title">Lịch sử và mục tiêu phát triển</h1>
+                <h1 className="title-1">Lịch sử và mục tiêu phát triển</h1>
                 <p className="content">
                     Koifish được thành lập vào năm  2015, bắt đầu với niềm đam mê cá Koi và mong muốn mang đến những chú cá Koi thuần chủng, chất lượng cao từ Nhật Bản. Qua nhiều năm phát triển, chúng tôi đã mở rộng quy mô hoạt động, xây dựng mối quan hệ vững chắc với các trang trại cá Koi nổi tiếng tại Nhật Bản và không ngừng cải thiện dịch vụ để phục vụ khách hàng tốt nhất.<br></br>
                     Với mục tiêu trở thành nhà cung cấp cá Koi hàng đầu tại Việt Nam, Koifish cam kết mang đến không chỉ những chú Koi đẹp, khỏe mạnh, mà còn tạo nên những trải nghiệm đáng nhớ cho khách hàng. Chúng tôi không ngừng đầu tư vào công nghệ và nâng cao chất lượng dịch vụ để đạt đến sự hoàn thiện trong mỗi bước phát triển.<br></br>
                 </p>
-                <h1 className="title">Cam kết uy tín</h1>
+                <h1 className="title-2">Cam kết uy tín</h1>
                 <p className="content">
                     Nguồn gốc rõ ràng: Tất cả cá Koi tại Koifish đều được nhập khẩu từ các trang trại uy tín nhất Nhật Bản, đảm bảo độ thuần chủng và chất lượng cao.<br></br>
                     Chất lượng vượt trội: Cá Koi của chúng tôi được nuôi dưỡng trong điều kiện tốt nhất, được kiểm tra kỹ lưỡng về sức khỏe trước khi đến tay khách hàng.<br></br>
