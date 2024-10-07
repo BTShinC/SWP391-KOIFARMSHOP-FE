7efa24c07a25f24917130c8f4a84512ca2f9f3ef
BTShinC <sangp3328@gmail.com>
2024-10-08 00:20:23 +0700

    fix banner khi deloy

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,16 +1,10 @@
 import logo from "/public/images/logo.svg";
-import bannerImage from "/public/images/banner.svg"; // Import your banner image
 import "./index.scss";
 
 function Banner({ title }) {
   return (
     <div className="banner-wraper">
-      <div
-        className="banner-container"
-        style={{
-          backgroundImage: `url(${bannerImage})`, // Only dynamically set the background image
-        }}
-      >
+      <div className="banner-container">
         <img src={logo} alt="Logo" className="logo" />
         <h2 className="shop-name">{title}</h2>
       </div>
