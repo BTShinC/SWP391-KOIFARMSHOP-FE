8abb9a3acbba8accb86507b17fd0daf25275bd93
BTShinC <sangp3328@gmail.com>
2024-10-25 13:12:51 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "Building app..."
 npm run build
 echo "Deploy files to server..."
-scp -r dist/* root@103.90.227.69:/var/www/html/
+# scp -r dist/* root@103.90.227.69:/var/www/html/
+scp -r dist/* root@103.90.227.69:/var/www/SWP391-KOIFARMSHOP-FE/src/
 echo "Done!"
